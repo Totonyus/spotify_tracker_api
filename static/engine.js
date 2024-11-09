710a953d41c12daeb20b917b9f3f4631fdb39d70
Totonyus <mail@totonyus.ovh>
2024-11-09 22:40:05 +0100

    Add navigation menu

--- static/engine.js
+++ static/engine.js
@@ -8,14 +8,14 @@ socket.addEventListener("message", (event) => {
 
     if (scan_running) {
         dom_element.innerHTML = `<span class="category_item"><span>Running scan</span> ➤ <span>${status.current_artist + status.current_show} on ${status.total_artists + status.total_shows}</span></span>`;
-    }else{
+    } else {
         dom_element.innerHTML = `<span class="category_item"><span>Scan complete</span> ➤ <span><a class="api_link_reference" href="#">Reload page</a></span></span>`;
         location.reload();
     }
 });
 
 socket.addEventListener("close", (event) => {
-    if (!scan_running){
+    if (!scan_running) {
         return
     }
 
@@ -25,7 +25,7 @@ socket.addEventListener("close", (event) => {
 });
 
 socket.addEventListener("error", (event) => {
-    if (!scan_running){
+    if (!scan_running) {
         return
     }
 
@@ -33,3 +33,23 @@ socket.addEventListener("error", (event) => {
 
     dom_element.innerHTML = `<span class="category_item error"><span>An error occurred during scan</span> ➤ <span><a class="api_link_reference" href="/refresh">/refresh</a></span></span>`;
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menu_dom_element = document.getElementById('menu');
+
+    if (menu_dom_element) {
+        menu_dom_element.innerHTML = `
+            <span><a class="api_link_reference" href="/">/</a> </span>
+            <span><a class="api_link_reference" href="/artists">/artists</a> (${menu_dom_element.dataset.nb_artists}) </span>
+            <span><a class="api_link_reference" href="/releases">/releases</a>  (${menu_dom_element.dataset.nb_releases}) </span>
+            <span><a class="api_link_reference" href="/shows">/shows</a>  (${menu_dom_element.dataset.nb_shows}) </span>
+            <span><a class="api_link_reference" href="/episodes">/episodes</a>  (${menu_dom_element.dataset.nb_episodes}) </span>
+            `
+
+        const burger_menu_dom_element = document.getElementById('burger_menu')
+        burger_menu_dom_element.addEventListener('click', function () {
+            menu_dom_element.classList.toggle('hidden')
+        })
+    }
+})
+
