35055c80661f6ff4a7d0af923db0b6e2ced0bea8
Totonyus <mail@totonyus.ovh>
2025-07-26 22:04:46 +0200

    Custom RSS feed generator

--- rss_feed_generator.py
+++ rss_feed_generator.py
@@ -1,50 +1,19 @@
+import jinja2
 from datetime import datetime
-from rfeed import *
-
 import spotify_api_helpers as api
 
 def generate_feed():
-    feed_items = []
+    loader = jinja2.FileSystemLoader('./templates')
+    env = jinja2.Environment(autoescape=True, loader=loader)
 
-    releases = api.get_releases()
+    template = env.get_template('rss_feed.xml.j2')
+    render = template.render({
+        'api': api,
+        'releases': api.get_releases(),
+        'episodes': api.get_episodes(),
+        'datetime': datetime
+    })
 
-    for item in releases:
-        artists_str = []
-
-        artists = item.get('artists')
-
-        for artist in artists:
-            artists_str.append(artist.get('name'))
-
-        feed_items.append(Item(
-            title=f'{item.get("artists")[0].get("name")} - {item.get("name")}',
-            link=item.get('external_urls').get('spotify'),
-            description=f'New {item.get("album_group")} with {item.get("total_tracks")} tracks. Featuring {", ".join(artists_str)}.',
-            author='Spotify tracker api',
-            guid=Guid(f'release_{item.get("id")}'),
-            pubDate=datetime.fromtimestamp(item.get(api.get_parameters().get("default_sorting")))
-        ))
-
-    episodes = api.get_episodes()
-    for item in episodes:
-        feed_items.append(Item(
-            title=f'{item.get("name")}',
-            link=item.get('external_urls').get('spotify'),
-            description=f'New episode of {round(item.get("duration_ms") / 1000 / 60)} minutes. Description : {item.get("description")}',
-            author='Spotify tracker api',
-            guid=Guid(f'episode_{item.get("id")}'),
-            pubDate=datetime.fromtimestamp(item.get(api.get_parameters().get("default_sorting")))
-        ))
-
-    feed = Feed(
-        title="Spotify Tracker",
-        link=f'{api.get_parameters().get("application_url")}/static/feed',
-        description='The rss feed to follow your artists and podcasts releases',
-        language='en-US',
-        lastBuildDate=datetime.now(),
-        items=feed_items
-    )
-
-    file = open('static/feed', 'w')
-    file.write(feed.rss())
-    file.close()
+    rss_feed = open('static/feed', 'w')
+    rss_feed.write(render)
+    rss_feed.close()
